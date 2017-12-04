8ca0a5e0bcf507ae50b0d717f561fcc637f80957
Matt Mason <mmason@breadturtle.local>
2017-12-03 21:13:26 -0800

    Add day 4, pre-cleanup

---
+++ 2017/day4/part2.py
@@ -0,0 +1,35 @@
+
+def main(data):
+	num_valid = 0
+	for line in data.split("\n"):
+		if is_valid(line):
+			num_valid += 1
+	return num_valid
+
+
+
+def is_valid(passphrase):
+	data = {}
+	for word in passphrase.split():
+		word = ''.join(sorted(word))
+		if word in data:
+			return False
+		data[word] = True
+	return True
+
+
+def get_input():
+    print("Give your input. EOF (CTRL+D) to end:")
+    data = []
+    while True:
+        try:
+            data.append(raw_input())
+        except EOFError:
+            break
+    return "\n".join(data)
+
+if __name__ == '__main__':
+    while True:
+        data = get_input()
+        checksum = main(data)
+        print "Num valid is: {}".format(checksum)
