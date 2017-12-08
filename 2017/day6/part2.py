122784290150a547e2842c1ff6f0b2477a3d7eff
Matt Mason <mason.g.matt@gmail.com>
2017-12-07 19:46:19 -0800

    Add day 6

---
+++ 2017/day6/part2.py
@@ -0,0 +1,20 @@
+import operator
+def main(input_data):
+    numbers = [int(x) for x in input_data.split()]
+    seen = {}
+    count = 0
+    while True:
+        index, value = max(enumerate(numbers), key=operator.itemgetter(1))
+        numbers[index] = 0
+        for i in range(1, value + 1):
+            numbers[(index+i)%len(numbers)] += 1
+        count += 1
+        if str(numbers) in seen:
+            break
+        seen[str(numbers)] = count
+    return count - seen[str(numbers)]
+
+if __name__ == '__main__':
+    while True:
+        input_data = raw_input('Challenge input: ')
+        print main(input_data)
